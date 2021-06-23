ac5208f7c646f64f05debf3db589b9783acba1ee
Steve Yu <jw.yu@aftership.com>
2021-06-23 17:42:03 +0800

    fix picture not show

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
     <main>
       <h1>Steve Yu's Gatsby WebSite</h1>
       <StaticImage
-        src="http://localhost:3000/static/media/profile.b272f5f5.png"
+        src="https://steveyuowo.com/static/media/profile.b272f5f5.png"
         alt="steve yu's animation profile"
         width="200"
         height="200"
